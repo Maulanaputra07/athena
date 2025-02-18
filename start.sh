7cdf0bd72491e31298362b1fbf3c4f67e5eb7fff
Maulanaputra07 <maulanaaputraaa077@gmail.com>
2025-02-18 14:47:12 +0700

    #13

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Menjalankan Rasa dengan API dan CORS, dan actions di background
-rasa run --enable-api --cors '*' &
+rasa run --enable-api --cors '*' --port 5005 &
 rasa run actions
+wait
